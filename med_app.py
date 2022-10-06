7aea34ed474f2c93489b1fa24cb56f9258aff7d9
Egraf99 <egorkhodin@gmail.com>
2022-10-06 13:27:37 +0300

    ошибка сохраняется в отдельный файл

--- med_app.py
+++ med_app.py
@@ -17,7 +17,14 @@ def main():
 def excepthook(exc_type, exc_value, exc_tb):
     tb = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
     print(tb)
-    QtWidgets.QApplication.quit()             # !!! если вы хотите, чтобы событие завершилось
+    write_exception_in_file(tb)
+    # QtWidgets.QApplication.quit()             # !!! если вы хотите, чтобы событие завершилось
+
+
+def write_exception_in_file(text):
+    file = open('error.txt', 'w')
+    file.write(text)
+    file.close()
 
 
 sys.excepthook = excepthook
